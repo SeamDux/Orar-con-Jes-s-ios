9bc02defffcc6bb68028befd6ba52b499e21cb84
SeamDux <seba.merino@duocuc.cl>
2025-10-02 10:47:52 -0300

    Ajustes de lectio divina

--- app.config.js
+++ app.config.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     name: 'Orar con Jesús',
     slug: 'orarconjesus',
-    version: '1.0.2',
+    version: '1.0.3',
     orientation: 'portrait',
     icon: './assets/images/android-chrome-512x512.png',
     userInterfaceStyle: 'light',
